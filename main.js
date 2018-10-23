05e9f4802dc4ba12bb918f7f70710afc67600e59
chase-stevens <chasestevenspersonal@gmail.com>
2018-10-23 15:48:01 -0700

    refactored code -- NOTE: preset no longer loads to main gradient

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
+// Defining preset gradients
+const PRESET_GRADIENTS = {
+  peach: {
+    angle: 135,
+    start_color: "#FFD3A5",
+    end_color: "#FD6585"
+  },
+  misty_morning: {
+    angle: 135,
+    start_color: "#C2FFD8",
+    end_color: "#465EFB"
+  },
+  limeaid: {
+    angle: 135,
+    start_color: "#FFF720",
+    end_color: "#3CD500"
+  }
+};
+
+// Setting main gradient values
 let current_gradient = {
   angle: 45,
   start_color: "#ff0000",
   end_color: "#0000ff",
 };
 
-document.onload = set_gradient_elements(render_gradient(
+// Loading Page
+document.onload = render_gradient(set_gradient_rule(
   current_gradient.angle,
   current_gradient.start_color,
   current_gradient.end_color
 ));
 
+// Collecting values from the gradient form
 function collect_gradient_data() {
   let gradient_form = document.getElementById("gradient_form");
   current_gradient.angle = gradient_form[2].value;
@@ -17,53 +39,30 @@ function collect_gradient_data() {
   current_gradient.end_color = gradient_form[1].value;
 }
 
-//  background-image: linear-gradient(45deg, red, blue);
-
-function render_gradient(angle, start_color, end_color) {
+// Makes a css gradient rule from values
+function set_gradient_rule(angle, start_color, end_color) {
   let gradient_value = `linear-gradient(${angle}deg, ${start_color}, ${end_color})`;
   return(gradient_value);
 }
 
-function set_gradient_elements(gradient) {
+// Displays gradient and gradient code
+function render_gradient(gradient) {
   document.getElementById("gradient").style.backgroundImage = gradient;
   document.getElementById("gradient-css-code").innerHTML = "background-image: " + gradient + ";";
 }
 
+// Combines form value grabbing, defining rule, and rendering gradient & code
 function create_gradient(){
   let els = collect_gradient_data();
-  let gradient_rule = render_gradient(
+  let gradient_rule = set_gradient_rule(
     current_gradient.angle,
     current_gradient.start_color,
     current_gradient.end_color
   );
-  set_gradient_elements(gradient_rule);
-  console.log(current_gradient);
+  render_gradient(gradient_rule);
 }
 
-function load_gradient_preset(gradient_element){
-  // loading gradient
-  let el = document.getElementById(gradient_element);
-  let style = window.getComputedStyle(el);
-  let gradient = style.getPropertyValue('background-image');
-  // rendering gradient to page elements
-  console.log(gradient);
-
-  parse_gradient(gradient);
-}
-
-/*
-function parse_gradient(gradient_rule){
-  let regex_rule = /linear-gradient\((.*)deg, (#.*|[rgb].*), (#.*|[rgb].*)\)/;
-  let parse_gradient = gradient_rule.match(regex_rule);
-  let x = parse_gradient[1];
-  let y = parse_gradient[2];
-  let z = parse_gradient[3];
-  document.getElementById("angle").value = x;
-  document.getElementById("start_color").value = y;
-  document.getElementById("end_color").value = z;
-  //return gradient_elements;
-}
-*/
+// Copies gradient code to user clipboard
 function copy_to_clipboard(){
   /* Get the text field */
   let copyText = document.getElementById("gradient-css-code").innerHTML;
