87e199ddc6e804890693251ac35df2dc3c0b237a
chase-stevens <chasestevenspersonal@gmail.com>
2018-11-07 17:34:42 -0700

    added linted and refactored code

--- main.js
+++ main.js
@@ -2,61 +2,61 @@
 const PRESET_GRADIENTS = {
   peach: {
     angle: 135,
-    start_color: "#FFD3A5",
-    end_color: "#FD6585"
+    start_color: '#FFD3A5',
+    end_color: '#FD6585',
   },
   misty_morning: {
     angle: 135,
-    start_color: "#C2FFD8",
-    end_color: "#465EFB"
+    start_color: '#C2FFD8',
+    end_color: '#465EFB',
   },
   limeaid: {
     angle: 135,
-    start_color: "#FFF720",
-    end_color: "#3CD500"
+    start_color: '#FFF720',
+    end_color: '#3CD500',
   },
   glimmer: {
     angle: 135,
-    start_color: "#FAB2FF",
-    end_color: "#1904E5",
+    start_color: '#FAB2FF',
+    end_color: '#1904E5',
   },
   sunrise: {
     angle: 135,
-    start_color: "#FFA8A8",
-    end_color: "#FCFF00",
+    start_color: '#FFA8A8',
+    end_color: '#FCFF00',
   },
   sunset: {
     angle: 135,
-    start_color: "#EECE13",
-    end_color: "#B210FF"
+    start_color: '#EECE13',
+    end_color: '#B210FF',
   },
   lemon: {
     angle: 135,
-    start_color: "#FDEB71",
-    end_color: "#F8D800",
+    start_color: '#FDEB71',
+    end_color: '#F8D800',
   },
   lavender: {
     angle: 135,
-    start_color: "#F5CBFF",
-    end_color: "#C346C2",
+    start_color: '#F5CBFF',
+    end_color: '#C346C2',
   },
   seafoam: {
     angle: 135,
-    start_color: "#79F1A4",
-    end_color: "#0E5CAD"
+    start_color: '#79F1A4',
+    end_color: '#0E5CAD',
   },
   velvet: {
     angle: 135,
-    start_color: "#3B2667",
-    end_color: "#BC78EC"
-  }
+    start_color: '#3B2667',
+    end_color: '#BC78EC',
+  },
 };
 
 // Setting main gradient values
 let current_gradient = {
   angle: 45,
-  start_color: "#ff0000",
-  end_color: "#0000ff",
+  start_color: '#ff0000',
+  end_color: '#0000ff',
 };
 
 // Loading Page
@@ -68,7 +68,7 @@ document.onload = render_gradient(set_gradient_rule(
 
 // Collecting values from the gradient form
 function collect_gradient_data() {
-  let gradient_form = document.getElementById("gradient_form");
+  let gradient_form = document.getElementById('gradient_form');
   current_gradient.angle = gradient_form[2].value;
   current_gradient.start_color = gradient_form[0].value;
   current_gradient.end_color = gradient_form[1].value;
@@ -76,18 +76,21 @@ function collect_gradient_data() {
 
 // Makes a css gradient rule from values
 function set_gradient_rule(angle, start_color, end_color) {
-  let gradient_value = `linear-gradient(${angle}deg, ${start_color}, ${end_color})`;
-  return(gradient_value);
+  let gradient_value = `linear-gradient(${angle}deg,
+                          ${start_color},
+                          ${end_color})`;
+  return (gradient_value);
 }
 
 // Displays gradient and gradient code
 function render_gradient(gradient) {
-  document.getElementById("gradient").style.backgroundImage = gradient;
-  document.getElementById("gradient-css-code").innerHTML = "background-image: " + gradient + ";";
+  document.getElementById('gradient').style.backgroundImage = gradient;
+  document.getElementById('gradient-css-code').innerHTML = 'background-image: '
+                                                            + gradient + ';';
 }
 
 // Combines form value grabbing, defining rule, and rendering gradient & code
-function create_gradient(){
+function create_gradient() {
   let els = collect_gradient_data();
   let gradient_rule = set_gradient_rule(
     current_gradient.angle,
@@ -117,9 +120,9 @@ function load_preset_gradient(preset_gradient){
 }
 
 // Copies gradient code to user clipboard
-function copy_to_clipboard(){
+function copy_to_clipboard() {
   /* Get the text field */
-  let copyText = document.getElementById("gradient-css-code").innerHTML;
+  let copyText = document.getElementById('gradient-css-code').innerHTML;
 
   const el = document.createElement('textarea');
   el.value = copyText;
@@ -132,5 +135,5 @@ function copy_to_clipboard(){
   document.body.removeChild(el);
 
   /* Alert the copied text */
-  alert("Copied the text: " + copyText);
+  alert('Copied the text: ' + copyText);
 }
